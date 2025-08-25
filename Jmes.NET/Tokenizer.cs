1c0a037cd88c1bc7faaf465871d4830b8d898a34
Justin <justin@justin.directory>
2025-08-24 21:26:32 -0400

    feat(tokenizer): add basic token types

--- Jmes.NET/Tokenizer.cs
+++ Jmes.NET/Tokenizer.cs
@@ -49,19 +49,74 @@ public sealed class Tokenizer
 	public List<TokenIndex> Tokenize()
 	{
 		var tokens = new List<TokenIndex>();
-		do
+
+		while (MoveNext())
 		{
 			switch (_enumerator.Current)
 			{
 				case char c when char.IsAsciiLetter(c) || c == '_':
 					tokens.Add((_index, ConsumeIdentifier()));
 					break;
+				case '.':
+					tokens.Add((_index, new DotToken()));
+					break;
+				case '*':
+					tokens.Add((_index, new StarToken()));
+					break;
+				case '@':
+					tokens.Add((_index, new AtToken()));
+					break;
+				case '&':
+					tokens.Add((_index, ConsumeMatchedOr<AndToken, AmpersandToken>('&')));
+					break;
+				case '>':
+					tokens.Add((_index, ConsumeMatchedOr<GteToken, GtToken>('=')));
+					break;
+				case '<':
+					tokens.Add((_index, ConsumeMatchedOr<LteToken, LtToken>('=')));
+					break;
+				case '!':
+					tokens.Add((_index, ConsumeMatchedOr<NeqToken, NotToken>('=')));
+					break;
+				case '(':
+					tokens.Add((_index, new LParenToken()));
+					break;
+				case ')':
+					tokens.Add((_index, new RParenToken()));
+					break;
+				case ',':
+					tokens.Add((_index, new CommaToken()));
+					break;
+				case ':':
+					tokens.Add((_index, new ColonToken()));
+					break;
+				case ']':
+					tokens.Add((_index, new RBracketToken()));
+					break;
+				case '{':
+					tokens.Add((_index, new LBraceToken()));
+					break;
+				case '}':
+					tokens.Add((_index, new RBraceToken()));
+					break;
 			}
-		} while (MoveNext());
+		}
 
 		tokens.Add((_index, new EofToken()));
 
 		return tokens;
+	}
+
+	private IToken ConsumeMatchedOr<TMatch, TNotMatch>(char toMatch)
+		where TMatch : IToken, new()
+		where TNotMatch : IToken, new()
+	{
+		if (_enumerator.Peek() == toMatch)
+		{
+			MoveNext();
+			return new TMatch();
+		}
+		return new TNotMatch();
 	}
 
 	private IdentifierToken ConsumeIdentifier()
