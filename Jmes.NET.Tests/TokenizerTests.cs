613eb10291bd66ef113abf0899bbaa1d46c0b05e
Justin <justin@justin.directory>
2025-08-26 21:11:44 -0400

    chore(tests): add basic tokenization tests

--- Jmes.NET.Tests/TokenizerTests.cs
+++ Jmes.NET.Tests/TokenizerTests.cs
@@ -2,6 +2,78 @@
 
 public class TokenizerTests
 {
+	/// <summary>
+	/// Tests for non-complex tokens, that don't require any custom logic.
+	/// </summary>
 	[Fact]
-	public void HandlesTokenization() { }
+	public void HandlesBasicTokens()
+	{
+		// . * @ ] { } ( ) ,
+		Assert.Equal(
+			Tokenizer.Tokenize("."),
+			[(0, JmesToken.Make(JmesTokenType.Dot)), (1, JmesToken.Make(JmesTokenType.Eof))]
+		);
+
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.Star)), (1, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("*")
+		);
+
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.At)), (1, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("@")
+		);
+
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.RBracket)), (1, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("]")
+		);
+
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.LBrace)), (1, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("{")
+		);
+
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.RBrace)), (1, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("}")
+		);
+
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.LParen)), (1, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("(")
+		);
+
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.RParen)), (1, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize(")")
+		);
+
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.Comma)), (1, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize(",")
+		);
+	}
+
+	[Fact]
+	public void HandlesLeftBrackets()
+	{
+		// expect flatten
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.Flatten)), (2, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("[]")
+		);
+
+		// expect filter
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.Filter)), (2, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("[?")
+		);
+
+		// expect lbracket
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.LBracket)), (1, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("[")
+		);
+	}
 }
