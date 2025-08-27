f5a530f8eca5be40796a020c9e8105af1dd77c0b
Justin <justin@justin.directory>
2025-08-27 17:48:09 -0400

    feat(tests): add more unit tests

--- Jmes.NET.Tests/TokenizerTests.cs
+++ Jmes.NET.Tests/TokenizerTests.cs
@@ -76,4 +76,110 @@ public class TokenizerTests
 			Tokenizer.Tokenize("[")
 		);
 	}
+
+	[Fact]
+	public void HandlesPipeAndOr()
+	{
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.Pipe)), (1, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("|")
+		);
+
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.Or)), (2, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("||")
+		);
+	}
+
+	[Fact]
+	public void HandlesAmpersand()
+	{
+		// &
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.Ampersand)), (1, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("&")
+		);
+
+		// && (and)
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.And)), (2, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("&&")
+		);
+	}
+
+	[Fact]
+	public void HandlesComparisonOperators()
+	{
+		// gt
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.Gt)), (1, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize(">")
+		);
+
+		// gte
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.Gte)), (2, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize(">=")
+		);
+
+		// lt
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.Lt)), (1, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("<")
+		);
+
+		// lte
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.Lte)), (2, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("<=")
+		);
+	}
+
+	[Fact]
+	public void HandlesEqualityOperators()
+	{
+		// eq
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.Eq)), (2, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("==")
+		);
+
+		// neq
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.Neq)), (2, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("!=")
+		);
+
+		// not
+		Assert.Equal(
+			[(0, JmesToken.Make(JmesTokenType.Not)), (1, JmesToken.Make(JmesTokenType.Eof))],
+			Tokenizer.Tokenize("!")
+		);
+	}
+
+	[Fact]
+	public void InvalidEqualFails()
+	{
+		Assert.Throws<TokenizationException>(() => Tokenizer.Tokenize("="));
+	}
+
+	[Fact]
+	public void IgnoresWhitespace()
+	{
+		Assert.Equal(
+			[
+				(5, JmesToken.Make(JmesTokenType.Dot)),
+				(7, JmesToken.Make(JmesTokenType.LParen)),
+				(8, JmesToken.Make(JmesTokenType.Eof)),
+			],
+			Tokenizer.Tokenize(" \t\n\r\t. (")
+		);
+	}
+
+	[Fact]
+	public void InvalidCharacterFails()
+	{
+		Assert.Throws<TokenizationException>(() => Tokenizer.Tokenize("#"));
+		Assert.Throws<TokenizationException>(() => Tokenizer.Tokenize("~"));
+	}
 }
