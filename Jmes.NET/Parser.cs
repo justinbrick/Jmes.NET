a7a2ad6eff6a7e8ca00ed148a8bca2f4fe048c8c
Justin <justin@justin.directory>
2025-09-14 13:40:22 -0400

    feat(parsing): add method stubs

--- Jmes.NET/Parser.cs
+++ Jmes.NET/Parser.cs
@@ -34,9 +34,21 @@ public sealed class Parser(Queue<(long, JmesToken)> queue)
 		return MoveNextWithPosition().Item2;
 	}
 
-	private JmesToken Peek()
+	private JmesToken Peek(int offset = 0)
 	{
-		return queue.TryPeek(out var token) ? token.Item2 : JmesToken.Make(JmesTokenType.Eof);
+		if (offset == 0)
+		{
+			return queue.TryPeek(out var token) ? token.Item2 : JmesToken.Make(JmesTokenType.Eof);
+		}
+
+		try
+		{
+			return queue.ElementAt(offset).Item2;
+		}
+		catch (ArgumentOutOfRangeException)
+		{
+			return JmesToken.Make(JmesTokenType.Eof);
+		}
 	}
 
 	private AstNode Evaluate(int rbp)
@@ -73,6 +85,17 @@ public sealed class Parser(Queue<(long, JmesToken)> queue)
 			case JmesTokenType.Literal:
 				return new AstLiteral { Offset = position, Value = token.Value };
 			case JmesTokenType.LBracket:
+				switch (Peek().Type)
+				{
+					case JmesTokenType.Number:
+					case JmesTokenType.Colon:
+						return ParseIndex();
+					case JmesTokenType.Star when Peek(1).Type == JmesTokenType.RBracket:
+						MoveNext();
+						return ParseWildcardIndex(new AstIdentity { Offset = position });
+					default:
+						return ParseMultiSelectList();
+				}
 			case JmesTokenType.Flatten:
 			case JmesTokenType.LBrace:
 			case JmesTokenType.Ampersand:
@@ -90,6 +113,20 @@ public sealed class Parser(Queue<(long, JmesToken)> queue)
 		throw new NotImplementedException();
 	}
 
+	/// <summary>
+	/// Parses a <see href="https://jmespath.org/specification.html#wildcard-expressions">projection expression</see>, defined as lhs into rhs
+	/// </summary>
+	/// <remarks>
+	/// The RHS of a projection can one of
+	/// <list type="bullet">
+	///		<item>a dot (.) followed by an identifier</item>
+	///		<item>a bracket ([) followed by an index or slice</item>
+	///		<item>a filter ([?) followed by an expression</item>
+	/// </list>
+	/// </remarks>
+	/// <param name="lbp">the binding power of the left-hand side expression</param>
+	/// <returns>a node representing the right-hand side of a projection expression</returns>
+	/// <exception cref="ParsingException"></exception>
 	private AstNode ParseProjection(int lbp)
 	{
 		switch (Peek().Type)
@@ -107,6 +144,11 @@ public sealed class Parser(Queue<(long, JmesToken)> queue)
 		}
 	}
 
+	private AstNode ParseIndex()
+	{
+		throw new NotImplementedException();
+	}
+
 	/// <summary>
 	/// When given a wildcard (*), parses a projection expression.
 	/// </summary>
@@ -121,5 +163,48 @@ public sealed class Parser(Queue<(long, JmesToken)> queue)
 			Left = new AstObjectValues { Offset = _index, Node = lhs },
 			Right = rhs,
 		};
+	}
+
+	private AstNode ParseWildcardIndex(AstNode lhs)
+	{
+		throw new NotImplementedException();
+	}
+
+	private AstNode ParseMultiSelectList()
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Equivalent to jmes-rs parse_kvp
+	/// </summary>
+	private AstHashMap ParseHashMap()
+	{
+		throw new NotImplementedException();
+	}
+
+	private AstProjection ParseFilter(AstNode lhs)
+	{
+		throw new NotImplementedException();
+	}
+
+	private AstFlatten ParseFlatten(AstNode lhs)
+	{
+		throw new NotImplementedException();
+	}
+
+	private AstComparison ParseComparison(AstComparisonOperator operand, AstNode lhs)
+	{
+		throw new NotImplementedException();
+	}
+
+	private AstNode ParseDot(int lbp)
+	{
+		throw new NotImplementedException();
+	}
+
+	private AstNode ParseList(JmesToken sentinel)
+	{
+		throw new NotImplementedException();
 	}
 }
