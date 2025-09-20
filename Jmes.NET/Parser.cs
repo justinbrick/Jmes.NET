f71e2eacd6d0eb7db3d85d1e189864385ca14e93
Justin <justin@justin.directory>
2025-09-20 12:11:24 -0400

    feat(parsing): more fleshed led & nud

--- Jmes.NET/Parser.cs
+++ Jmes.NET/Parser.cs
@@ -97,12 +97,33 @@ public sealed class Parser(Queue<(long, JmesToken)> queue)
 						return ParseMultiSelectList();
 				}
 			case JmesTokenType.Flatten:
+				return ParseFlatten(new AstIdentity { Offset = position });
 			case JmesTokenType.LBrace:
+				return ParseHashMap();
 			case JmesTokenType.Ampersand:
+				return new AstExpressionRef
+				{
+					Offset = position,
+					Expression = Evaluate(JmesToken.GetLeftBindingPower(JmesTokenType.Ampersand)),
+				};
 			case JmesTokenType.Not:
+				return new AstNot
+				{
+					Offset = position,
+					Operand = Evaluate(JmesToken.GetLeftBindingPower(JmesTokenType.Not)),
+				};
 			case JmesTokenType.Filter:
+				return ParseFilter(new AstIdentity { Offset = position });
 			case JmesTokenType.LParen:
-				throw new NotImplementedException();
+				var expr = Evaluate(0);
+				var next = MoveNext();
+				if (next.Type != JmesTokenType.RParen)
+				{
+					throw new ParsingException(
+						$"Expected ')', found {next.Type} at position {_index}"
+					);
+				}
+				return expr;
 			default:
 				throw new ParsingException($"Unexpected token {token.Type} at position {position}");
 		}
@@ -110,14 +131,102 @@ public sealed class Parser(Queue<(long, JmesToken)> queue)
 
 	private AstNode LeftDenotation(AstNode left)
 	{
-		throw new NotImplementedException();
+		var (position, token) = MoveNextWithPosition();
+		switch (token.Type)
+		{
+			case JmesTokenType.Dot:
+				if (Peek().Type == JmesTokenType.Star)
+				{
+					MoveNext();
+					return ParseWildcardProjection(left);
+				}
+
+				return new AstSubExpression
+				{
+					Offset = position,
+					Left = left,
+					Right = ParseDot(JmesToken.GetLeftBindingPower(JmesTokenType.Dot)),
+				};
+			case JmesTokenType.LBracket:
+				switch (Peek().Type)
+				{
+					case JmesTokenType.Number:
+					case JmesTokenType.Colon:
+						return new AstSubExpression
+						{
+							Offset = position,
+							Left = left,
+							Right = ParseIndex(),
+						};
+					case JmesTokenType.Star:
+						MoveNext();
+						return ParseWildcardIndex(left);
+					default:
+						throw new ParsingException(
+							$"Unexpected token {Peek().Type} after '[' at position {_index}. Expected number, colon, or '*'"
+						);
+				}
+			case JmesTokenType.Or:
+				return new AstOr
+				{
+					Offset = position,
+					Left = left,
+					Right = Evaluate(JmesToken.GetLeftBindingPower(JmesTokenType.Or)),
+				};
+			case JmesTokenType.And:
+				return new AstAnd
+				{
+					Offset = position,
+					Left = left,
+					Right = Evaluate(JmesToken.GetLeftBindingPower(JmesTokenType.And)),
+				};
+			case JmesTokenType.Pipe:
+				return new AstSubExpression
+				{
+					Offset = position,
+					Left = left,
+					Right = Evaluate(JmesToken.GetLeftBindingPower(JmesTokenType.Pipe)),
+				};
+			case JmesTokenType.LParen:
+				if (left is not AstField field)
+				{
+					throw new ParsingException(
+						$"Expected function name before '(', found {left.GetType().Name} at position {position}"
+					);
+				}
+
+				return new AstFunction
+				{
+					Offset = position,
+					Name = field.FieldName,
+					Arguments = ParseList(JmesTokenType.RParen),
+				};
+			case JmesTokenType.Flatten:
+				return ParseFlatten(left);
+			case JmesTokenType.Filter:
+				return ParseFilter(left);
+			case JmesTokenType.Eq:
+				return ParseComparison(AstComparisonOperator.Eq, left);
+			case JmesTokenType.Neq:
+				return ParseComparison(AstComparisonOperator.Neq, left);
+			case JmesTokenType.Gt:
+				return ParseComparison(AstComparisonOperator.Gt, left);
+			case JmesTokenType.Gte:
+				return ParseComparison(AstComparisonOperator.Gte, left);
+			case JmesTokenType.Lt:
+				return ParseComparison(AstComparisonOperator.Lt, left);
+			case JmesTokenType.Lte:
+				return ParseComparison(AstComparisonOperator.Lte, left);
+			default:
+				throw new ParsingException($"Unexpected token {token.Type} at position {position}");
+		}
 	}
 
 	/// <summary>
 	/// Parses a <see href="https://jmespath.org/specification.html#wildcard-expressions">projection expression</see>, defined as lhs into rhs
 	/// </summary>
 	/// <remarks>
-	/// The RHS of a projection can one of
+	/// The RHS of a projection can be one of
 	/// <list type="bullet">
 	///		<item>a dot (.) followed by an identifier</item>
 	///		<item>a bracket ([) followed by an index or slice</item>
@@ -165,14 +274,26 @@ public sealed class Parser(Queue<(long, JmesToken)> queue)
 		};
 	}
 
-	private AstNode ParseWildcardIndex(AstNode lhs)
+	private AstProjection ParseWildcardIndex(AstNode lhs)
 	{
-		throw new NotImplementedException();
+		var next = MoveNext();
+		return next.Type != JmesTokenType.RBracket
+			? throw new ParsingException($"Expected ']', found {next.Type}")
+			: new AstProjection
+			{
+				Offset = _index,
+				Left = lhs,
+				Right = ParseProjection(JmesToken.GetLeftBindingPower(JmesTokenType.Star)),
+			};
 	}
 
-	private AstNode ParseMultiSelectList()
+	/// <summary>
+	/// Parses a multi-select list, e.g. [a, b, c]
+	/// </summary>
+	/// <returns>the parsed AST list node</returns>
+	private AstList ParseMultiSelectList()
 	{
-		throw new NotImplementedException();
+		return new AstList { Offset = _index, Items = ParseList(JmesTokenType.RBracket) };
 	}
 
 	/// <summary>
@@ -180,6 +301,42 @@ public sealed class Parser(Queue<(long, JmesToken)> queue)
 	/// </summary>
 	private AstHashMap ParseHashMap()
 	{
+		var values = new List<(string Key, AstNode Value)>();
+		var notDone = true;
+		while (notDone)
+		{
+			var expectedKey = MoveNext();
+			switch (expectedKey.Type)
+			{
+				case JmesTokenType.QuotedIdentifier:
+				case JmesTokenType.Identifier:
+					if (MoveNext().Type != JmesTokenType.Colon)
+					{
+						throw new ParsingException(
+							$"Expected ':' after key in object at position {_index}"
+						);
+					}
+
+					values.Add(((string)expectedKey.Value!, Evaluate(0)));
+					break;
+				default:
+					throw new ParsingException(
+						$"Expected identifier or quoted identifier for key, found {expectedKey.Type} at position {_index}"
+					);
+			}
+
+			switch (MoveNext().Type)
+			{
+				case JmesTokenType.Comma:
+					continue;
+				case JmesTokenType.RBrace:
+					notDone = false;
+					break;
+				default:
+					throw new ParsingException($"Expected ',' or '}}' in object, found {_index}");
+			}
+		}
+
 		throw new NotImplementedException();
 	}
 
@@ -203,8 +360,31 @@ public sealed class Parser(Queue<(long, JmesToken)> queue)
 		throw new NotImplementedException();
 	}
 
-	private AstNode ParseList(JmesToken sentinel)
+	/// <summary>
+	/// Parses a comma-separated list of expressions until the sentinel token is reached.
+	/// </summary>
+	/// <param name="sentinel">the token type to stop parsing at</param>
+	/// <returns>a list of parsed AST nodes</returns>
+	/// <exception cref="ParsingException">a trailing comma was encountered before the sentinel token is encountered</exception>
+	private List<AstNode> ParseList(JmesTokenType sentinel)
 	{
-		throw new NotImplementedException();
+		var items = new List<AstNode>();
+		while (Peek().Type != sentinel)
+		{
+			items.Add(Evaluate(0));
+			if (Peek().Type == JmesTokenType.Comma)
+			{
+				MoveNext();
+				if (Peek().Type == sentinel)
+				{
+					throw new ParsingException(
+						$"Trailing comma before {sentinel} at position {_index}"
+					);
+				}
+			}
+		}
+		// Clear the sentinel
+		MoveNext();
+		return items;
 	}
 }
--- Jmes.NET/Syntax/AstComparison.cs
+++ Jmes.NET/Syntax/AstComparison.cs
@@ -26,9 +26,14 @@ public sealed class AstComparison : AstNode
 	/// <param name="token">the token to get the operator for</param>
 	/// <returns>the comparison operator that corresponds to the token</returns>
 	/// <exception cref="ArgumentOutOfRangeException">the <paramref name="token"/> is not a valid comparison operator token</exception>
-	public static AstComparisonOperator GetOperator(JmesToken token)
+	public static AstComparisonOperator FromToken(JmesToken token)
 	{
-		return token.Type switch
+		return FromTokenType(token.Type);
+	}
+
+	public static AstComparisonOperator FromTokenType(JmesTokenType type)
+	{
+		return type switch
 		{
 			JmesTokenType.Eq => AstComparisonOperator.Eq,
 			JmesTokenType.Neq => AstComparisonOperator.Neq,
@@ -37,9 +42,9 @@ public sealed class AstComparison : AstNode
 			JmesTokenType.Gt => AstComparisonOperator.Gt,
 			JmesTokenType.Gte => AstComparisonOperator.Gte,
 			_ => throw new ArgumentOutOfRangeException(
-				nameof(token),
-				token,
-				"Expected comparison operator token."
+				nameof(type),
+				type,
+				"Expected comparison operator token type."
 			),
 		};
 	}
